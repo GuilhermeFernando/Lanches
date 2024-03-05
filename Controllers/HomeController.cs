6f7c9cc0938f49aeaf706c18fd3fc0abb90ea713
Guilherme Machado <guifermach@hotmail.com>
2024-03-05 20:47:01 -0300

    Projeto Lanches

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using Lanches.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Lanches.Controllers
+{
+    public class HomeController : Controller
+    {
+       
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Dem()
+        {
+            return View();
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/Categoria.cs
@@ -0,0 +1,12 @@
+﻿namespace Lanches.Models;
+
+public class Categoria
+{
+    public int CategoriaId { get; set; }
+    public string CategriaNome { get; set; }
+    public string Descricao { get; set; }
+
+    public List<Lanche> Lanches { get; set; }
+
+
+}
